50ceca70e6af3140e975f2170f05680ed19eee79
Oleksandr Litash <litaash92@gmail.com>
2024-03-01 22:27:14 +0200

    added deploy

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: 'https://github.com/Litaash/digiturn.git',
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        about: 'about.html'
+      }
+    }
+  }
+});
